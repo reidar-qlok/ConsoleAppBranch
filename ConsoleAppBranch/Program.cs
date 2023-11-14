6e7732c9ab1e5ec68ca91b5ba11aa10a37860b27
Reidar Nilsen <4reidar@gmail.com>
2023-11-14 10:19:47 +0100

    Add project files.

---
+++ ConsoleAppBranch/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleAppBranch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skapa referens av en employee och lägg in lite data
+            // Skapa en metod som startar programmet
+        }
+    }
+}
